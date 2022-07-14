831689d1531f135e181c86b46678333c91fc6b50
Hemantdhurve <hemantdhurve007@gmail.com>
2022-07-14 23:06:25 +0530

    Added UC1 SnakenLadder file

---
+++ UC1SnakenLadder/UC1SnakenLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC1SnakeNladder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //define constants
+            //Player 1 = P1 and Position 1 = Pos1
+            const int P1 = 1;
+            int Pos1 = 0;
+            Console.WriteLine("Initial position of P1 is :" + "" + Pos1);
+
+        }
+    }
+}
